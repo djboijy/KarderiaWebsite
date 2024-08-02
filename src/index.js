fb89ee8cf25a42f633e82317266b950007e10353
BoijyProgrammerX <166648999+djboijy@users.noreply.github.com>
2024-08-02 19:59:28 +0300

    Removed some modules which aren't needed

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './i18n'; // Import the i18n configuration
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
